204f9902bd0d5117b1ebc905c55f4f1350155fc2
Jacioneide <jacioneide.michelle@escolar.ifrn.edu.br>
2025-12-04 15:05:23 -0300

    Atualização

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from models import Tarefa, GerenciadorTarefas
 
 app = Flask(__name__)
+app.secret_key = "minha_chave_super_secreta_123"
+
 gerenciador = GerenciadorTarefas()
 
 @app.route('/')
+def inicio():
+    return redirect(url_for('cadastro'))
+
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro():
+    if request.method == 'POST':
+        return redirect(url_for('login'))
+    return render_template('cadastro.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return redirect(url_for('index'))
+    return render_template('login.html')
+
+@app.route('/index')
 def index():
     tarefas = gerenciador.obter_tarefas()
     disciplinas = gerenciador.obter_disciplinas()
@@ -14,10 +32,17 @@ def index():
 def disciplina(nome_disciplina):
     tarefas = gerenciador.obter_tarefas_por_disciplina(nome_disciplina)
     disciplinas = gerenciador.obter_disciplinas()
-    return render_template('disciplina.html', 
-                         tarefas=tarefas, 
-                         disciplinas=disciplinas, 
-                         disciplina_selecionada=nome_disciplina)
+    return render_template(
+        'disciplina.html', 
+        tarefas=tarefas, 
+        disciplinas=disciplinas, 
+        disciplina_selecionada=nome_disciplina
+    )
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("login"))
 
 @app.route('/novo', methods=['GET', 'POST'])
 def novo():
